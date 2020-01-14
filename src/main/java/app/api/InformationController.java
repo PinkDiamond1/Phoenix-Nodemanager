92ba0832e4280a8f9a01068e279664a710a59a84
Artem Eger <ae@atr0x.com>
2020-01-14 16:52:07 +0100

    witness api endpoint

--- src/main/java/app/api/InformationController.java
+++ src/main/java/app/api/InformationController.java
@@ -2,6 +2,7 @@ package app.api;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import message.request.IRPCMessage;
 import message.request.ProducerListType;
@@ -14,14 +15,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 @Controller
 @RequestMapping(ApiPaths.API)
@@ -85,9 +87,51 @@ public class InformationController {
     @RequestMapping(value = ApiPaths.WITNESS, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String getWitnesses() {
+        final HashMap<String, Object> responseMap = new HashMap<>();
+        final MongoCursor<Document> witnesses = mongoClient.getDatabase("apex")
+                .getCollection("witnessStatus").find().limit(1).iterator();
+        final MongoCursor<Document> producer = mongoClient.getDatabase("apex")
+                .getCollection("block")
+                .find().sort(new Document("height", -1))
+                .limit(1).iterator();
+        if(witnesses.hasNext() && producer.hasNext()){
+            final String currentProducer = producer.next().getString("producer");
+            final List<HashMap> witnessList = witnesses.next().getList("witnesses", HashMap.class);
+            witnessList.forEach(witness -> {
+                final String address = (String) witness.get("addr");
+                responseMap.put("name", witness.get("name"));
+                responseMap.put("addr", witness.get("addr"));
+                responseMap.put("voteCounts", witness.get("voteCounts"));
+                responseMap.put("longitude", witness.get("longitude"));
+                responseMap.put("latitude", witness.get("latitude"));
+                if(address.equals(currentProducer)){
+                    responseMap.put("radius", 12);
+                    responseMap.put("fillKey", witness.get("yellowFill"));
+                } else {
+                    responseMap.put("radius", 4);
+                    responseMap.put("fillKey", witness.get("redFill"));
+                }
+            });
+            try {
+                return jacksonWriter.getStringFromRequestObject(responseMap);
+            } catch (JsonProcessingException e) {
+                return "{}";
+            }
+        }
+        return "{}";
+    }
 
-        return getCoreMessage(new GetProducersCmd(ProducerListType.ALL));
-
+    @RequestMapping(value = ApiPaths.WITNESS_REFRESH, method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void refreshWitnesses() {
+        final Document witnessEntry = Document.parse(getCoreMessage(new GetProducersCmd(ProducerListType.ALL)));
+        if(!witnessEntry.isEmpty()){
+            final MongoCollection<Document> collection = mongoClient.getDatabase("apex")
+                    .getCollection("witnessStatus");
+            final MongoCursor<Document> iter = collection.find().limit(1).iterator();
+            if(iter.hasNext()) collection.findOneAndReplace(iter.next(), witnessEntry);
+            else collection.insertOne(witnessEntry);
+        }
     }
 
     private String getCoreMessage(final IRPCMessage msg){
