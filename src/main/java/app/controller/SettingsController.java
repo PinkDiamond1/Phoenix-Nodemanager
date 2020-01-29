0cee5a43bb4b215f5d6077cab8bb2c4c708363df
Artem Eger <ae@atr0x.com>
2020-01-29 10:09:25 +0100

    add wipe

--- src/main/java/app/controller/SettingsController.java
+++ src/main/java/app/controller/SettingsController.java
@@ -4,24 +4,48 @@ import app.config.ApplicationPaths;
 import app.entity.ApplicationUser;
 import app.process.ProcessExecutor;
 import app.repository.ApplicationUserRepository;
+import app.settings.ConfigurationFileService;
+import app.settings.component.ChainComponent;
+import app.settings.component.RootComponent;
+import app.settings.component.SettingsField;
+import com.mongodb.MongoClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 @Controller
 @RequestMapping("/" + ApplicationPaths.SETTINGS_PAGE)
 public class SettingsController {
 
+    private final Logger log = LoggerFactory.getLogger(SettingsController.class);
+
     @Autowired
     private ProcessExecutor processExecutor;
 
+    @Autowired
+    private MongoClient mongoClient;
 
     @Autowired
     private ApplicationUserRepository userRepository;
+
+    @Value("${app.settings}")
+    private String settingsPath;
+
+    @Value("${app.settings.default}")
+    private String settingsDefaultPath;
 
     @GetMapping
     public String getSettings(){
@@ -50,6 +74,30 @@ public class SettingsController {
                 userRepository.save(applicationUser);
                 return ApplicationPaths.LOGOUT_PATH;
             }
+        }
+
+        return ApplicationPaths.SETTINGS_PATH;
+
+    }
+
+    @PostMapping(params = "action=wipe")
+    public String wipeData() {
+
+        try {
+            final RootComponent component = Files.isReadable(Paths.get(settingsPath)) ?
+                    ConfigurationFileService.loadSettings(settingsPath) :
+                    ConfigurationFileService.loadSettings(ResourceUtils.getFile(settingsDefaultPath).getAbsolutePath());
+            final ArrayList<String> dirs = new ArrayList<>();
+            final ChainComponent chainComponent = (ChainComponent) component.getComponents().get(ChainComponent.NAME);
+            dirs.add((String) chainComponent.getBlockBase().get(SettingsField.CHAIN_DIR));
+            dirs.add((String) chainComponent.getForkBase().get(SettingsField.CHAIN_DIR));
+            dirs.add((String) chainComponent.getDataBase().get(SettingsField.CHAIN_DIR));
+            dirs.add("peers");
+            dirs.add("logs");
+            processExecutor.wipe(dirs);
+            mongoClient.getDatabase("apex").drop();
+        } catch (IOException e) {
+            log.error("Wipe command failed");
         }
 
         return ApplicationPaths.SETTINGS_PATH;
